e68929dd9a5bb7fa012576f90e220cc3925696d9
timmoh-king <timothykariuki43@gmail.com>
2023-06-27 15:21:15 +0300

    Javascript

--- intro-to_react/src/App.js
+++ intro-to_react/src/App.js
@@ -5,20 +5,59 @@ import Total from "./components/Total"
 
 const App = () => {
   const course = 'Half Stack application development'
-  const part1 = 'Fundamentals of React'
-  const exercises1 = 10
-  const part2 = 'Using props to pass data'
-  const exercises2 = 7
-  const part3 = 'State of a component'
-  const exercises3 = 14
+  const parts = [
+    {
+      name: 'Fundamentals of React',
+      exercises: 10
+    },
+    {
+      name: 'Using props to pass data',
+      exercises: 7
+    },
+    {
+      name: 'State of a component',
+      exercises: 14
+    }
+  ]
+
+  const arto = {
+    name: 'Arto Hellas',
+    age: 35,
+    education: 'PhD',
+    greet: function() {
+      console.log('hello, my name is ' + this.name + 'I am ' + this.age + ' years')
+    },
+  }
+  arto.growOlder = function() {
+    this.age += 1
+  }
+  arto.greet()
+  arto.growOlder()
+  arto.greet()
+
+  class Person {
+    constructor(name, age) {
+      this.name = name
+      this.age = age
+    }
+    greet() {
+      console.log('hello, my name is ' + this.name)
+    }
+  }
+  
+  const adam = new Person('Adam Ondra', 29)
+  adam.greet()
+  
+  const janja = new Person('Janja Garnbret', 23)
+  janja.greet()
 
   return (
     <div>
       <Header course={course} />
-      <Content part={part1} exercise={exercises1} />
-      <Content part={part2} exercise={exercises2} />
-      <Content part={part3} exercise={exercises3} />
-      <Total exercise1={exercises1} exercise2={exercises2} exercise3={exercises3} />
+      <Content part={parts[0].name} exercise={parts[0].exercises} />
+      <Content part={parts[1].name} exercise={parts[1].exercises} />
+      <Content part={parts[2].name} exercise={parts[2].exercises} />
+      <Total exercise1={parts[0].exercises} exercise2={parts[1].exercises} exercise3={parts[2].exercises} />
     </div>
   )
 }
